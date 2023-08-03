545e05ad3a738e55e66ad21deca76ec88da75177
JulioPeloz <julio.pelo78@gmail.com>
2023-08-03 17:45:08 -0600

    Crear eventos en la base de datos

--- src/hooks/useCalendarStore.js
+++ src/hooks/useCalendarStore.js
@@ -1,10 +1,12 @@
 import { useDispatch, useSelector } from "react-redux";
 import { onAddNewEvent, onDeleteEvent, onSetActiveEvent, onUpdateEvent } from "../store";
+import { calendarApi } from "../api";
 
 export const useCalendarStore = () => {
 
     const dispatch = useDispatch();
     const {events, activeEvent} = useSelector(state => state.calendar);
+    const { user } = useSelector(state => state.auth);
 
     const setActiveEvent = ( calendarEvent ) => {
         dispatch( onSetActiveEvent( calendarEvent ) )
@@ -12,12 +14,15 @@ export const useCalendarStore = () => {
 
     const startSavingEvent = async(calendarEvent) => {
 
+        //TODO: Update event
         if(calendarEvent._id){
             //Actualizando
             dispatch(onUpdateEvent({ ...calendarEvent }))
         }else{
             //creando
-            dispatch( onAddNewEvent({...calendarEvent, _id: new Date().getTime()}))
+            const { data } = await calendarApi.post('/events', calendarEvent);
+            console.log(data);
+            dispatch( onAddNewEvent({...calendarEvent, id: data.evento.id, user}))
         }
     }
 
